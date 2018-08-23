3cbc507b691c14eecda79c5c4fa6a80b7cd4e52f
Rabvc4 <chill_zero@hotmail.com>
2018-08-23 16:19:45 -0500

    Change enum to be official list of item types

--- src/main/java/com/example/rngesus/models/enumerations/ItemType.java
+++ src/main/java/com/example/rngesus/models/enumerations/ItemType.java
@@ -2,21 +2,29 @@ package com.example.rngesus.models.enumerations;
 
 public enum ItemType {
 
-    WEAPON ("Weapon"),
-    ARMOR ("Armor"),
-    TOOL ("Tool"),
-    CURRENCY ("Currency"),
-    TRADE_GOOD ("Trade Good"),
-    UTILITY ("Utility");
+    ARMOR ("Armor", "Armor"),
+    WEAPON ("Weapon", "Weapons"),
+    GEAR ("Adventuring Gear", "Adventuring Gear"),
+    TOOL ("Tool", "Tools"),
+    CURRENCY ("Currency", "Currencies"),
+    MOUNT ("Mount", "Mounts"),
+    VEHICLE ("Vehicle", "Vehicles"),
+    TRADE_GOOD ("Trade Good", "Trade Goods"),
+    UTILITY ("Utility", "Utilities");
 
     private final String name;
+    private final String plural;
 
-    ItemType(String name) {
+    ItemType(String name, String plural) {
         this.name = name;
+        this.plural = plural;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getPlural() {
+        return plural;
+    }
 }
