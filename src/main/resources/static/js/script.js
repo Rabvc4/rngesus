99afb218d526778000ccb397f9c8dd2d3dfec6c2
Rabvc4 <chill_zero@hotmail.com>
2018-08-30 13:13:48 -0500

    Add .get jQuery for dynamically adding data to modal.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,6 +1,5 @@
 //$(function(){
 //    var current = location.pathname;
-//    if (window.console) console.log('foo');
 //    $('#nav a.nav-link').each(function(){
 //        var $this = $(this);
 //        // if the current path is like this link, make it active
@@ -9,4 +8,31 @@
 //        }
 //    })
 //})
+
+//$(function(){
 //
+//    $('.table .item').on('click',function(event){
+//        event.preventDefault();
+//        if (window.console) console.log('foo');
+//
+//        $('.myForm #itemModal').modal();
+//    });
+//});
+
+$(function(){
+    $('.table .item').click(function(event){
+        event.preventDefault();
+        var href = $(this).attr('href');
+
+        $('.itemForm #description').text("Success");
+
+        $.get(href, function(item, status){
+            $('.itemForm #name').text(item.name);
+            $('.itemForm #description').text(item.description);
+            $('.itemForm #cost').text(item.cost);
+            $('.itemForm #weight').text(item.weight);
+        });
+
+        $('.itemForm #itemModal').modal();
+    });
+});
