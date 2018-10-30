3494b7887ffc4d65f353b36a45d774280bbd740c
Rabvc4 <chill_zero@hotmail.com>
2018-10-30 16:21:21 -0500

    Add method for finding class by name

--- src/main/java/com/example/rngesus/models/data/ClassDao.java
+++ src/main/java/com/example/rngesus/models/data/ClassDao.java
@@ -5,8 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Repository
 public interface ClassDao extends CrudRepository<CharacterClass, Integer> {
+
+    List<CharacterClass> findByName(String name);
+
 }
