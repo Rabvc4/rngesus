8c6e0f8409a0f0a1be4dab7e50853afb57fb4c25
Rabvc4 <chill_zero@hotmail.com>
2018-10-30 16:22:02 -0500

    Add path for class details

--- src/main/java/com/example/rngesus/controllers/ClassController.java
+++ src/main/java/com/example/rngesus/controllers/ClassController.java
@@ -1,12 +1,14 @@
 package com.example.rngesus.controllers;
 
+import com.example.rngesus.models.CharacterClass;
+import com.example.rngesus.models.User;
 import com.example.rngesus.models.data.ClassDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("class")
@@ -20,9 +22,27 @@ public class ClassController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, @RequestParam(defaultValue = "0") int id) {
+        if (id != 0) {
+            model.addAttribute("class", classDao);
+            model.addAttribute("title", "Classes");
+
+            return "class/index";
+
+        }
         model.addAttribute("title", "Classes");
         model.addAttribute("classes", classDao.findAll());
+
         return "class/index";
+    }
+
+    @RequestMapping(value = "{className}", method = RequestMethod.GET)
+    public String details(Model model, @PathVariable String className) {
+
+        CharacterClass characterClass = classDao.findByName(className).get(0);
+        model.addAttribute("title", characterClass.getName());
+        model.addAttribute("class", characterClass);
+
+        return "class/details";
     }
 
 }
