950443103748d408758c3e4d1153377ce0c6e3e1
Rabvc4 <chill_zero@hotmail.com>
2018-11-06 14:24:43 -0600

    Combine Melee/Ranged to update enum

--- src/main/java/com/example/rngesus/models/enumerations/WeaponProficiency.java
+++ src/main/java/com/example/rngesus/models/enumerations/WeaponProficiency.java
@@ -2,12 +2,9 @@ package com.example.rngesus.models.enumerations;
 
 public enum WeaponProficiency {
 
-    SIMPLE_MELEE ("Simple Melee Weapon", "Simple Melee Weapons"),
-    MARTIAL_MELEE ("Martial Melee Weapon", "Martial Melee Weapons"),
-    EXOTIC_MELEE ("Exotic Melee Weapon", "Exotic Melee Weapons"),
-    SIMPLE_RANGED ("Simple Ranged Weapon", "Simple Ranged Weapons"),
-    MARTIAL_RANGED ("Martial Ranged Weapon", "Martial Ranged Weapons"),
-    EXOTIC_RANGED ("Exotic Ranged Weapon", "Exotic Ranged Weapons");
+    SIMPLE_WEAPON ("Simple Weapon", "Simple Weapons"),
+    MARTIAL_WEAPON ("Martial Weapon", "Martial Weapons"),
+    EXOTIC_WEAPON ("Exotic Weapon", "Exotic Weapons");
 
     private final String name;
     private final String pluralName;
