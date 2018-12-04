d439ec826e40c6bbc41f03fa570b9b65de051bea
Rabvc4 <chill_zero@hotmail.com>
2018-12-04 16:41:11 -0600

    Refactor DurationType to be used in conjunction with a static value to allow more open-endedness

--- src/main/java/com/example/rngesus/models/enumerations/DurationType.java
+++ src/main/java/com/example/rngesus/models/enumerations/DurationType.java
@@ -5,14 +5,9 @@ public enum DurationType {
     REACTION ("Reaction"),
     BONUS_ACTION ("Bonus Action"),
     ACTION ("Action"),
-    ONE_MINUTE ("1 Minute"),
-    TEN_MINUTES ("10 Minutes"),
-    ONE_HOUR ("1 Hour"),
-    EIGHT_HOURS ("8 Hours"),
-    ONE_DAY ("24 Hours"),
-    SEVEN_DAYS ("7 Days"),
-    TEN_DAYS ("10 Days"),
-    THIRTY_DAYS ("30 Days"),
+    MINUTE ("Minute"),
+    HOUR ("Hour"),
+    DAY ("Day"),
     INDEFINITE ("Until Stopped/Dispelled");
 
     private final String name;
