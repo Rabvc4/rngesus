e45943193afb4bf4a90b206384a8c46d727c879f
Rabvc4 <chill_zero@hotmail.com>
2018-12-05 16:31:17 -0600

    Add try/catch for out of bounds exceptions

--- src/main/java/com/example/rngesus/controllers/RaceController.java
+++ src/main/java/com/example/rngesus/controllers/RaceController.java
@@ -23,6 +23,22 @@ public class RaceController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, @RequestParam(defaultValue = "0") int id) {
 
+        if (id != 0) {
+            Race race = raceDao.findById(id).orElse(null);
+            if (race != null) {
+                model.addAttribute("title", race.getName());
+                model.addAttribute("race", race);
+
+                return "race/details";
+            } else {
+                model.addAttribute("title", "No Such Race");
+                model.addAttribute("message", "That race does not yet exist, maybe that's where you come in?");
+
+                return "error";
+            }
+
+        }
+
         model.addAttribute("title", "Races");
         model.addAttribute("races", raceDao.findAll());
 
@@ -34,11 +50,19 @@ public class RaceController {
     @RequestMapping(value = "{raceName}", method = RequestMethod.GET)
     public String details(Model model, @PathVariable String raceName) {
 
-        Race race = raceDao.findByName(raceName).get(0);
-        model.addAttribute("title", race.getName());
-        model.addAttribute("race", race);
+        try {
+            Race race = raceDao.findByName(raceName).get(0);
+            model.addAttribute("title", race.getName());
+            model.addAttribute("race", race);
 
-        return "race/details";
+            return "race/details";
+        } catch(IndexOutOfBoundsException exception) {
+            model.addAttribute("title", "No Such Race");
+            model.addAttribute("message", "That race does not yet exist, maybe that's where you come in?");
+
+            return "error";
+        }
+
     }
 
 }
